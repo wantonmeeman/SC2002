02d22c8fbf5feb13aab438193517df96fcba7a9e
Yong Ming Xuan <yongmx3455@gmail.com>
2025-04-03 22:32:28 +0800

    Testing pushing to github in Flat.java

--- Code/Flat.java
+++ Code/Flat.java
@@ -4,7 +4,7 @@ public class Flat {
     private int totalUnits;
     //private int availableUnits;
     //Is there available units?
-    //Testing pushing to GitHub
+    //commit testing
     private float price;
 
     public Flat(String type, int totalUnits, float price) {
