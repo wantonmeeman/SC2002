cb93c4bcc193b6a2cc5b52bd222feb1fdfa49929
jhm13j <100938873+jhm13j@users.noreply.github.com>
2025-04-13 19:11:58 +0800

    Add files via upload

---
+++ Code/Pages/LogoutPage.java
@@ -0,0 +1,22 @@
+
+/**
+ * Class that handles user logout functionality.
+ */
+public class LogoutPage {
+
+    /**
+     * Default constructor for LogoutPage.
+     */
+    public LogoutPage() {
+        // Optional setup
+    }
+
+    /**
+     * Logs the user out of the system.
+     *
+     * @param userID The ID of the user logging out.
+     */
+    public void logout(String userID) {
+        System.out.println("User " + userID + " has been logged out.");
+    }
+}
